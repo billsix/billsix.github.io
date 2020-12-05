9862767f2078becf785c5a7009ad509d2d73189c
William Emerison Six <billsix@gmail.com>
2020-12-05 10:45:37 -0500

    updated

--- modelviewprojection/fixGithubIssue.sh
+++ modelviewprojection/fixGithubIssue.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+# after copying over the generated data from github.com/billsix/modelviewprojection/doc Make
+
+rm -rf static images sources
+mv _sources sources
+mv _images images
+mv _static static
+
 find . -iname '*.rst.txt' | xargs sed -i -e 's/_static\//static\//g'
 find . -iname '*.rst.txt' | xargs sed -i -e 's/_images\//images\//g'
+find . -iname '*.rst.txt' | xargs sed -i -e 's/_sources\//sources\//g'
 
 find . -iname '*.html' | xargs sed -i -e 's/_static\//static\//g'
 find . -iname '*.html' | xargs sed -i -e 's/_images\//images\//g'
+find . -iname '*.html' | xargs sed -i -e 's/_sources\//sources\//g'
 
 find . -iname '*.inv' | xargs sed -i -e 's/_static\//static\//g'
 find . -iname '*.inv' | xargs sed -i -e 's/_images\//images\//g'
+find . -iname '*.inv' | xargs sed -i -e 's/_sources\//sources\//g'
 
 find . -iname '*.js' | xargs sed -i -e 's/_static\//static\//g'
 find . -iname '*.js' | xargs sed -i -e 's/_images\//images\//g'
+find . -iname '*.js' | xargs sed -i -e 's/_sources\//sources\//g'
