466106e71e1c79f7b14e565dd8be843a4db48dd0
William Emerison Six <billsix@gmail.com>
2024-02-20 11:10:34 -0500

    updated content

--- modelviewprojection/fixGithubIssue.sh
+++ modelviewprojection/fixGithubIssue.sh
@@ -7,18 +7,22 @@ mv _sources sources
 mv _images images
 mv _static static
 
-find . -iname '*.rst.txt' | xargs sed -i -e 's/_static\//static\//g'
-find . -iname '*.rst.txt' | xargs sed -i -e 's/_images\//images\//g'
-find . -iname '*.rst.txt' | xargs sed -i -e 's/_sources\//sources\//g'
+# For MacOS to not have sed complain
+export LC_CTYPE=C
+export LANG=C
 
-find . -iname '*.html' | xargs sed -i -e 's/_static\//static\//g'
-find . -iname '*.html' | xargs sed -i -e 's/_images\//images\//g'
-find . -iname '*.html' | xargs sed -i -e 's/_sources\//sources\//g'
+find . -iname '*.rst.txt' | xargs sed -i -e 's@_static/@static/@g'
+find . -iname '*.rst.txt' | xargs sed -i -e 's@_images/@images/@g'
+find . -iname '*.rst.txt' | xargs sed -i -e 's@_sources/@sources/@g'
 
-find . -iname '*.inv' | xargs sed -i -e 's/_static\//static\//g'
-find . -iname '*.inv' | xargs sed -i -e 's/_images\//images\//g'
-find . -iname '*.inv' | xargs sed -i -e 's/_sources\//sources\//g'
+find . -iname '*.html' | xargs sed -i -e 's@_static/@static/@g'
+find . -iname '*.html' | xargs sed -i -e 's@_images/@images/@g'
+find . -iname '*.html' | xargs sed -i -e 's@_sources/@sources/@g'
 
-find . -iname '*.js' | xargs sed -i -e 's/_static\//static\//g'
-find . -iname '*.js' | xargs sed -i -e 's/_images\//images\//g'
-find . -iname '*.js' | xargs sed -i -e 's/_sources\//sources\//g'
+find . -iname '*.inv' | xargs sed -i -e 's@_static/@static/@g'
+find . -iname '*.inv' | xargs sed -i -e 's@_images/@images/@g'
+find . -iname '*.inv' | xargs sed -i -e 's@_sources/@sources/@g'
+
+find . -iname '*.js' | xargs sed -i -e 's@_static/@static/@g'
+find . -iname '*.js' | xargs sed -i -e 's@_images/@images/@g'
+find . -iname '*.js' | xargs sed -i -e 's@_sources/@sources/@g'
